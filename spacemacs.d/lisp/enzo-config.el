1ab857c5487b3902c80ce8a49fc98ff8e9702a21
Liu Enze <liuenze6516@gmail.com>
2016-08-13 15:35:41 +0800

    add spacemacs.d config

---
+++ spacemacs.d/lisp/enzo-config.el
@@ -0,0 +1,235 @@
+;;;###auto-load
+(defun enzo/run ()
+  "Use this to run single file in different file type."
+  (interactive)
+  (let* ((otherCmdMap `(("cpp" . "&& ./a.out")))
+         (suffixMap `(("py" . "python") ("rb" . "ruby") ("js" . "node") ("cpp" . "g++")
+                      ("sh" . "bash") ("ml" . "ocaml") ("vbs" . "cscript") ("c" . "c")
+                      ("hs" . "stack --install-ghc runghc") ("c++" . "c")))
+         (fName (buffer-file-name))
+         (fSuffix (file-name-extension fName))
+         (progName (cdr (assoc fSuffix suffixMap)))
+         (otherCmd (cdr (assoc fSuffix otherCmdMap)))
+         (cmdStr (concat progName " \""   fName "\" " otherCmd)))
+    (when (buffer-modified-p)
+      (when (y-or-n-p "Buffer modified.  Do you want to save first? ")
+        (save-buffer)))
+    (if progName
+        (progn
+          (message "Running…")
+          (shell-command cmdStr))
+      (message "No recognized program file suffix for this file."))))
+
+(setq *is-a-mac* (eq  system-type 'darwin))
+
+(defun get-usaco-path ()
+  "Get home path for usaco.  Consider in mac or linux."
+  (let* ((prefix `((t . "~") (nil . "~")))
+         (path (concat (cdr (assoc *is-a-mac* prefix)) "/work/usacontext/" )))
+    path))
+
+(defun get-folder-last-modify-date (folder)
+  "Return the FOLDER last modify date."
+  (substring
+   (shell-command-to-string (format "ls -ld %s | awk -F' '  '{ print $6 \" \"$7 }'" folder))
+   0 -1))
+
+(defun usaco-format (folder name type)
+  "Given FOLDER, NAME, &KEY TYPE, return ((NAME . NAME) (TYPE . TYPE) (FOLDER . FOLDER))."
+  (cons (concat name "-" (symbol-name type))
+        `(("name" . ,name)
+          ("folder" . ,folder)
+          ("type" . ,type))))
+
+(defun string/start-with (s begins)
+  "Return non-nil if string S start with BEGINS."
+  (cond ((>= (length s) (length begins))
+         (string-equal (substring s 0 (length begins)) begins))
+        (t nil)))
+
+(defun list-sub-directory (path)
+  "List all the directory below PATH.  Just the first level."
+  (let ((res '()))
+    (dolist (f (directory-files path))
+      (let ((name (concat path "/" f)))
+        (when (and (file-directory-p name)
+                   (not (string/start-with f ".")))
+          (setf res (push f res)))))
+    res))
+
+(defun get-done-project (path)
+  "Get all done project by path.  The done project list in $PATH/done/."
+  (let ((done-path (concat path "done/")))
+    (mapcar #'(lambda (name) (usaco-format (concat done-path name) name :DONE))
+            (list-sub-directory done-path))))
+
+(defun get-unfinished-project (path)
+  "Get all unfinished project by path.  The done project list in $PATH/done/."
+  (let ((done-path path))
+    (mapcar #'(lambda (name) (usaco-format (concat done-path name) name :TODO))
+            (cl-remove-if #'(lambda (name) (equal name "done")) (list-sub-directory done-path)))))
+
+(defun usaco-lookup ()
+  "Search for exsiting usaco programs and leave an add option."
+  (let ((path (get-usaco-path)))
+    (append
+     (list (usaco-format path "undefined" :CREATE))
+     (get-unfinished-project path)
+     (get-done-project path))))
+
+(defun actions-for-usaco (actions program)
+  "Return ACTIONS for selecting a PROGRAM."
+  (let ((type (cdr (assoc "type" program))))
+    (cond ((equal type :CREATE)
+           `(("CREATE" . uco-create)))
+          ((equal type :TODO)
+           `(("OPEN" . uco-display)
+             ("MARK-DONE" . uco-done)
+             ("REMOVE" . uco-remove)))
+          (t `(("OPEN" . uco-display)
+               ("MARK-UNDONE" . uco-undone))))))
+
+(defvar uco-helm-source-search
+  '((name . "USACO")
+    (candidates-process . usaco-lookup)
+    (action-transformer . actions-for-usaco)))
+
+;;;###auto-load
+(defun enzo/uco ()
+  "Use this to init the ${NAME} programming for usacontext."
+  (interactive)
+  (helm :sources '(uco-helm-source-search)
+        :buffer "*usaco-search*"
+        :candidate-number-limit 20))
+
+(defun uco-create (name)
+  "Create program in usaco for the input NAME."
+  (interactive "s please input the program name you want to create: ")
+  (let* ((sedcmd `((t . "sed -i '' ") (nil . "sed -i ")))
+         (path (get-usaco-path))
+         (folder (concat path name "/")))
+    (unless (file-exists-p folder)
+      (make-directory folder)
+      (shell-command (concat  "cp " path  "sample/sample.cpp " folder name ".cpp && "
+                              (cdr (assoc *is-a-mac* sedcmd)) "'s/#{name}/" name "/' " folder "*.cpp" )))
+    (uco-display `(("folder" . ,folder) ("name" . ,name)))))
+
+(defun uco-undone (program)
+  "Mark this PROGRAM to be undone."
+  (let* ((folder (cdr (assoc "folder" program)))
+         (dest (concat folder "/../../"))
+         (cmd (concat "mv " folder " " dest)))
+    (shell-command cmd)
+    (uco-clean program)))
+
+(defun slient-kill-buffers (regexp)
+  "Kill buffers matching REGEXP without asking for confirmation."
+  (interactive "sKill buffers matching this regular expression: ")
+  (cl-flet ((kill-buffer-ask (buffer) (kill-buffer buffer)))
+    (kill-matching-buffers regexp)))
+
+(defun uco-remove(program)
+  "Remove this PROGRAM."
+  (let ((folder (cdr (assoc "folder" program))))
+    (shell-command (concat "rm -rf " folder))))
+
+(defun uco-clean (program)
+  "Clear all the buffer of this PROGRAM and delete current frame."
+  (slient-kill-buffers (cdr (assoc "name" program)))
+  ;;(delete-frame)
+  )
+
+(defun uco-done (program)
+  "Mark this PROGRAM has been done."
+  (let* ((folder (cdr (assoc "folder" program)))
+         (dest (concat folder "/../done/"))
+         (cmd (concat "mv " folder " " dest)))
+    (shell-command cmd)
+    (uco-clean program)))
+
+(defun uco-display (program)
+  "Display for the exsiting PROGRAM."
+  (let* ((name (cdr (assoc "name" program)))
+         (folder (concat (cdr  (assoc "folder" program)) "/"))
+         (uco-cpp (concat folder name ".cpp"))
+         (uco-pro (concat folder name ".pr"))
+         (uco-in (concat folder name ".in"))
+         (uco-out (concat folder name ".out")))
+    (make-frame-command)
+    (switch-to-buffer (find-file-noselect uco-cpp))
+    (set-window-buffer (split-window-horizontally) (find-file-noselect uco-pro))
+    (windmove-right)
+    (set-window-buffer (split-window-below) (find-file-noselect uco-in))
+    (windmove-down)
+    (set-window-buffer (split-window-right) (find-file-noselect uco-out))
+    (windmove-left)
+    (toggle-frame-fullscreen)))
+
+(setq mac-command-modifier 'super)
+(global-set-key (kbd "C-s-f") 'toggle-frame-fullscreen)
+(global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
+
+;;;###auto-load
+(defun indent-buffer ()
+  "Use this to indent the whole file."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(menu-bar-mode -1)
+(tabbar-mode -1)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+(sp-use-paredit-bindings)
+(spacemacs/set-leader-keys
+  "oe" 'helm-find-files
+  "ob" 'helm-buffers-list
+  "ok" 'kill-buffer
+  "ow" 'save-buffer
+  "or" 'enzo/run
+  "oy" 'browse-kill-ring
+  "ou" 'enzo/uco
+  "on" 'helm-projectile-find-file
+  "oq" 'delete-frame
+  "of" 'indent-buffer
+  "ot" 'org-show-todo-tree
+  "oa" 'show-all
+  "o,f" 'ace-jump-mode
+  "o,F" 'ace-jump-char-mode
+  "o,w" 'ace-jump-word-mode)
+(define-key evil-normal-state-map (kbd ",") (lookup-key spacemacs-default-map (kbd "o")))
+(define-key evil-motion-state-map (kbd ",") (lookup-key spacemacs-default-map (kbd "o")))
+
+(defun enzo/setup-font (size ratio)
+  (when (display-graphic-p)
+    (let ((font (if *is-a-mac*
+                    "Hiragino Sans GB"
+                  "WenQuanYi Zen Hei") ))
+      (set-face-attribute 'default t :family "Sauce Code Powerline" :weight 'normal)
+      (set-face-attribute 'default nil :height size :weight 'normal)
+      (dolist (charset '(kana han symbol cjk-misc bopomofo))
+        (set-fontset-font t charset (font-spec :name font)))
+      (setq face-font-rescale-alist `((,font . ,ratio) ("文泉驿正黑" . 1.2))))))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (face-remap-add-relative 'default
+                                     :family "Sauce Code Powerline")))
+
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (with-selected-frame frame
+              (enzo/setup-font 150 1.2)
+              (when (and *is-a-mac* (display-graphic-p))
+                (ns-do-applescript "tell application \"Emacs\" to activate"))
+              )))
+
+(require 'pangu-spacing)
+(add-hook 'org-mode-hook (lambda () (pangu-spacing-mode 1)))
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)))
+
+(provide 'enzo-config)
---
+++ spacemacs.d/lisp/enzo-org.el
@@ -0,0 +1,168 @@
+(add-to-list 'safe-local-variable-values '(org-beamer-outline-frame-title . "目录"))
+(setq org-src-fontify-natively t)
+(setq org-export-allow-bind-keywords t)
+
+(defvar *beamer*
+  '("beamer"
+    "\\documentclass[presentation]{beamer}
+\\usepackage[no-math]{fontspec}
+\\usepackage[BoldFont,SlantFont,AutoFakeBold=true,AutoFakeSlant=true]{xeCJK}
+\\setCJKmainfont[BoldFont=FandolSong-Bold.otf,ItalicFont=FandolKai-Regular.otf]{FandolSong-Regular.otf}
+\\setCJKsansfont[BoldFont=FandolHei-Bold.otf]{FandolHei-Regular.otf}
+\\setCJKmonofont{FandolFang-Regular.otf}
+\\usefonttheme[stillsansseriflarge,stillsansserifsmall]{serif}
+\\usepackage{graphicx}
+\\usepackage{xcolor}
+\\usepackage{listings}
+\\defaultfontfeatures{Mapping=tex-text}
+\\usepackage{geometry}
+\\usepackage{verbatim}
+\\usepackage{fixltx2e}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{wrapfig}
+\\usepackage{rotating}
+\\usepackage[normalem]{ulem}
+\\usepackage{amsmath}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{amssymb}
+\\usepackage{hyperref}
+\\setlength{\\parindent}{0in}
+\\tolerance=1000
+     [NO-DEFAULT-PACKAGES]
+     [PACKAGES]
+     [EXTRA]
+"
+    ("\\section{%s}" . "\\section*{%s}")
+    ("\\subsection{%s}" . "\\subsection*{%s}")
+    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+  "Beamer org export latex config.")
+(defvar *book*
+  '("book"
+    "\\documentclass{book}"
+    ("\\section{%s}" . "\\section*{%s}")
+    ("\\subsection{%s}" . "\\subsection*{%s}")
+    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+  "Book org export latex config.")
+(defvar *article*
+  '("article"
+    "\\documentclass[11pt,a4paper]{article}
+\\usepackage{fontspec}
+\\usepackage{xeCJK}
+\\setCJKmainfont[BoldFont=FandolSong-Bold.otf,ItalicFont=FandolKai-Regular.otf]{FandolSong-Regular.otf}
+\\setCJKsansfont[BoldFont=FandolHei-Bold.otf]{FandolHei-Regular.otf}
+\\setCJKmonofont{FandolFang-Regular.otf}
+\\usepackage{graphicx}
+\\usepackage{xcolor}
+\\usepackage{listings}
+\\defaultfontfeatures{Mapping=tex-text}
+\\usepackage{geometry}
+\\usepackage{verbatim}
+\\usepackage{fixltx2e}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{wrapfig}
+\\usepackage{rotating}
+\\usepackage[normalem]{ulem}
+\\usepackage{amsmath}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{amssymb}
+\\usepackage{hyperref}
+\\usepackage{parskip}
+\\setlength{\\parindent}{15pt}
+\\usepackage{indentfirst}
+\\geometry{a4paper, textwidth=6.5in, textheight=10in,
+            marginparsep=7pt, marginparwidth=.6in}
+\\tolerance=1000
+\\pagestyle{empty}
+     [NO-DEFAULT-PACKAGES]
+     [NO-PACKAGES]
+ "
+    ("\\section{%s}" . "\\section*{%s}")
+    ("\\subsection{%s}" . "\\subsection*{%s}")
+    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+  "Acticle org export latex config.")
+
+(eval-after-load "org"
+  '(progn
+     (require 'ox-md nil t)
+     (require 'ox-latex)
+     (setq org-latex-pdf-process
+           '("xelatex -interaction nonstopmode %f"
+             "xelatex -interaction nonstopmode %f"))
+     (unless (boundp 'org-latex-classes)
+       (setq org-latex-classes nil))
+     (setq org-latex-listings 'listings)
+     ;;(setq org-latex-custom-lang-environments '((emacs-lisp "common-lispcode")))
+     (setq org-latex-listings-options
+           '(("frame" "single")
+             ("backgroundcolor" "\\color[gray]{0.95}")
+             ("identifierstyle" "\\ttfamily")
+             ("keywordstyle" "\\color[rgb]{0,0,1}")
+             ("commentstyle" "\\color[rgb]{0.133,0.545,0.133}")
+             ("stringstyle" "\\color[rgb]{0.627,0.126,0.941}")
+             ("basicstyle" "\\scriptsize")
+             ("extendedchars" "true")
+             ("breaklines" "true")
+             ("prebreak" "\\raisebox{0ex}[0ex][0ex]{\\ensuremath{\\hookleftarrow}}")
+             ("columns" "fixed")
+             ("keepspaces" "true")
+             ("showstringspaces" "false")
+             ("numbers" "left")
+             ("xleftmargin" ".25in")
+             ("xrightmargin" ".25in")
+             ("numberstyle" "\\tiny")))
+     (require 'ox-beamer)
+     (dolist (class (list *article* *book* *beamer*))
+       (add-to-list 'org-latex-classes
+                    class))))
+(setq org-confirm-babel-evaluate nil)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((lisp . t) (plantuml . t)))
+(setf plantuml-jar-path (expand-file-name "plantuml.jar" (file-name-directory user-init-file)))
+(setq org-plantuml-jar-path plantuml-jar-path)
+(setq org-descriptive-links nil)
+
+;; org 自动换行
+(add-hook 'org-mode-hook
+          (lambda ()
+            (turn-off-auto-fill)
+            (toggle-truncate-lines 0)))
+
+(defadvice org-html-paragraph (before org-html-paragraph-advice
+                                      (paragraph contents info) activate)
+  "Join consecutive Chinese lines into a single long line without unwanted space when exporting 'org-mode to html."
+  (let* ((origin-contents (ad-get-arg 1))
+         (fix-regexp "[[:multibyte:]]")
+         (fixed-contents
+          (replace-regexp-in-string
+           (concat
+            "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+    (ad-set-arg 1 fixed-contents)))
+
+(require 'tex)
+(require 'tex-buf)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(setq-default TeX-engine 'xetex)
+(setq-default TeX-PDF-mode t)
+;; set XeTeX mode in TeX/LaTeX
+(add-hook 'LaTeX-mode-hook
+          (lambda()
+            (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+            (turn-on-reftex)
+            (setq TeX-command-default "XeLaTeX")
+            (setq TeX-save-query nil)
+            (setq TeX-show-compilation t)))
+
+(provide 'enzo-org)
