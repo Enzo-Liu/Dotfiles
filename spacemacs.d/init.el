c4805f68abc7db77fee1e20f175960ccec3b8f7d
Liu Enze <liuenze6516@gmail.com>
2016-08-15 07:46:07 +0800

    add asm layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -40,6 +40,7 @@ values."
      syntax-checking
      version-control
      yaml
+     asm
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
