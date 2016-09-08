cb40adfff005222dc4e7e6ae89901101338b7dad
Enzo Liu <liuenze6516@gmail.com>
2016-09-08 22:24:16 +0800

    add rails mode

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -43,6 +43,8 @@ values."
      yaml
      asm
      html
+     react
+     ruby-on-rails
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -103,9 +105,9 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(solarized-light
+   dotspacemacs-themes '(solarized-dark
+                         solarized-light
                          spacemacs-dark
-                         solarized-dark
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
