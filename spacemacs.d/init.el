9b78c37d81cfa8a733a765096e39db8752445fed
Liu Enze <liuenze6516@gmail.com>
2017-02-05 15:15:57 +0800

    adjust default indentation for js mode

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -55,6 +55,7 @@ values."
                  ;; js2-mode
                  js2-basic-offset 2
                  ;; web-mode
+                 tab-width 2
                  css-indent-offset 2
                  web-mode-markup-indent-offset 2
                  web-mode-css-indent-offset 2
