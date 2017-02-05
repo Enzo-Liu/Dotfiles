3c8d7c7db2af0dc025403d3b37d18a6df0bd0abb
Liu Enze <liuenze6516@gmail.com>
2017-02-05 14:13:54 +0800

    remove emacs.d

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -51,7 +51,15 @@ values."
      org
      latex
      (javascript :variables
-                 js2-strict-missing-semi-warning nil)
+                 js2-strict-missing-semi-warning nil
+                 ;; js2-mode
+                 js2-basic-offset 2
+                 ;; web-mode
+                 css-indent-offset 2
+                 web-mode-markup-indent-offset 2
+                 web-mode-css-indent-offset 2
+                 web-mode-code-indent-offset 2
+                 web-mode-attr-indent-offset 2)
      osx
      (haskell :variables
               haskell-enable-hindent-style "gibiansky"
