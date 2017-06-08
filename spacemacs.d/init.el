ede9c927c5ad8bd847dcd0d04b0f2fc40b843214
Liu Enze <liuenze6516@gmail.com>
2017-06-08 17:29:30 +0800

    update

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -360,6 +360,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (require 'ansible-doc)
   (require 'org-page)
   (require 'enzo-org)
   (require 'enzo-java)
