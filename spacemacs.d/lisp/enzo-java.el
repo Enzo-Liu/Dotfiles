1ade4418ce19381f6dbe02e920c98f721d305e7b
Liu Enze <liuenze6516@gmail.com>
2020-04-05 10:10:56 +0800

    remove meghanada mode

--- spacemacs.d/lisp/enzo-java.el
+++ spacemacs.d/lisp/enzo-java.el
@@ -1,13 +1,7 @@
 (require 'meghanada)
 (add-hook 'java-mode-hook
           (lambda ()
-            (meghanada-mode t)
-            (gradle-mode t)
             (add-hook 'before-save-hook 'delete-trailing-whitespace)))
-
-(add-hook 'groovy-mode-hook
-          (lambda ()
-            (gradle-mode t)))
 
 (define-key evil-normal-state-map (kbd "C-]") (kbd "\\ M-."))
 (define-key evil-normal-state-map (kbd "C-o") (kbd "\\ M-,"))
